eac864449fa90ceb3e9f597bc6cf38f60ca5bf31
Angelika Tyborska <atyborska93@gmail.com>
2016-07-24 15:59:25 +0200

    Update brew install

--- brew_install.sh
+++ brew_install.sh
@@ -6,3 +6,13 @@ brew install nmap
 brew install postgresql
 brew install jsonpp
 brew install tree
+brew install elixir
+brew install sox
+brew install nginx
+brew install redis
+brew install postgresql
+brew install rabbitmq
+brew install mongodb
+brew install node
+brew install go
+brew install exercism

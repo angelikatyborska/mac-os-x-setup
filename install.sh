4312ad29554d343d2498a910c4dfd4ca774e19cc
Angelika Tyborska <atyborska93@gmail.com>
2016-08-24 07:31:45 +0200

    Fix git-aware-prompt installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Git aware prompt
-git clone git://github.com/jimeh/git-aware-prompt.git ~/.bash
+cd ~/.bash && git clone git://github.com/jimeh/git-aware-prompt.git && cd ~
 
 # RVM with latest Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
